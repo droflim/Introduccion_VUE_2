c67e7d95d39519074611b333c4dde9f922a6ae0a
droflim <estay.milford@gmail.com>
2021-07-22 18:21:16 -0400

    Uploaded Files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <ul>
+    <h1>Lista</h1>
+    <label>Tarea </label>
+    <input type="text" v-model="input" placeholder="Ingresa una nueva Tarea" style="width: 250px" />
+    <button class="btn btn-secondary" @click="agregarPersona">Agregar</button>
+    <h2>Crear una Lista</h2>
+    <li v-for="(item, index) in items" :key="index">{{ index }} {{ item }}</li>
+  </ul>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'app',
+  data: function () {
+    return {
+      input: '',
+      items: [],
+    }
   },
-};
-</script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  methods: {
+    agregarPersona() {
+      if (!this.input) {
+        return
+      }
+      this.items.push(this.input)
+
+      this.input = ''
+    },
+  },
 }
-</style>
+</script>
